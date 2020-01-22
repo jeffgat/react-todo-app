aa4ccc492721270a540c22fe204f8afb55173d23
jeffreygatbonton <jeff@creationcrate.com>
2020-01-21 22:56:39 -0500

    Updates

---
+++ precache-manifest.e87204cb6668d6f6c327a9d00fe9e560.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbd56e9f45611dd2607a2484d88eae0d",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "a98550a95d38208dbe23",
+    "url": "/react-todo-app/static/css/main.f7041bf3.chunk.css"
+  },
+  {
+    "revision": "70bb34a2d0069d6262c7",
+    "url": "/react-todo-app/static/js/2.46e70f52.chunk.js"
+  },
+  {
+    "revision": "a98550a95d38208dbe23",
+    "url": "/react-todo-app/static/js/main.d5a4813a.chunk.js"
+  },
+  {
+    "revision": "077a3ecc8ec082aa9fe1",
+    "url": "/react-todo-app/static/js/runtime~main.33c9f932.js"
+  }
+]);
